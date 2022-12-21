a84dfe5a2784f7554116e2eb989a957059a036f7
Nishwariya <nishwariya01@gmail.com>
2022-12-21 04:29:52 -0800

    Commited

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""square class main""
+
+
+class Square():
+    """square class"""
+
+    def __init(slef, size=None):
+    "" Instance of class Square
+    Args:
+    size (int): of side square
+    """
+    self._size = size
