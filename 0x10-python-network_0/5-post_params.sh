0321c7aa951958866462f334c435c063101f2cdf
nishwariya <nishwariya01@gmail.com>
2023-03-30 07:07:25 -0700

    Commited

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Take in URL, POST key:vals; Usage: ./5-post_params.sh 0.0.0.0:5000/route_6 ; echo ""
+# Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
 curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
 
