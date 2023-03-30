40f2c0266acbd2a5f8d2e23d6cf3d583547c1ddc
nishwariya <nishwariya01@gmail.com>
2023-03-30 06:51:43 -0700

    Commited

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -so /dev/null -w "%{http_code}" "$1"
+# Take in URL, display status code only; Usage: ./100-status_code.sh 0.0.0.0:5000/nop ; echo ""
+curl -o /dev/null -w '%{http_code}' -sLI "$1"
 
